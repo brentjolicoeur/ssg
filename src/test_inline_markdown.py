89040dee39653dcd6d6a1d482d37e0d90daf79e7
brentjolicoeur <brentjolicoeur@gmail.com>
2025-02-16 20:32:45 -0500

    Block Markdown

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,6 +1,6 @@
 import unittest
 
-from splitnodes import  split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link
+from inline_markdown import  split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link
 from textnode import TextNode, TextType
 
 
