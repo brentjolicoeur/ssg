6e755d34c5a9beab27c3af3bcf09c9e33f45e649
brentjolicoeur <brentjolicoeur@gmail.com>
2025-02-26 13:31:49 -0500

    Ch 5 L4 finished

--- src/main.py
+++ src/main.py
@@ -5,7 +5,6 @@ from generate_content import generate_pages_recursive, generate_page
 
 def main():
     clear_and_copy_directory('./static', './public')
-    #generate_page('./content/index.md', 'template.html', './public/index.html')
     generate_pages_recursive('./content', 'template.html', './public')
 
 def clear_and_copy_directory(source_path, destination_path):
