c902801a5c54c53ebca0bd967af70c424745d0a1
brentjolicoeur <brentjolicoeur@gmail.com>
2025-02-26 14:21:46 -0500

    push to GitHub pages

--- src/main.py
+++ src/main.py
@@ -1,11 +1,15 @@
-import os, shutil
+import os, shutil, sys
 
 from generate_content import generate_pages_recursive, generate_page
 
 
 def main():
-    clear_and_copy_directory('./static', './public')
-    generate_pages_recursive('./content', 'template.html', './public')
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+    else:
+        basepath = '/'
+    clear_and_copy_directory('./static', './docs')
+    generate_pages_recursive('./content', 'template.html', './docs', basepath)
 
 def clear_and_copy_directory(source_path, destination_path):
     if os.path.exists(destination_path):
